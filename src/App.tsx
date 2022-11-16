46ccbb449398dfce1f7eb28dcc43b4b0acb2a3ed
gokhansisman <pa1ngokhan@gmail.com>
2022-11-16 02:04:11 +0100

    Create initial GraphQL setup and query user

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
 import { useState } from 'react'
 import './App.css'
+import { ApolloClient, gql, useQuery } from "@apollo/client";
 
+const GET_USERS = gql`
+  query SearchUsers($userQuery: String!) {
+    search(query: $userQuery, type: USER, first: 20) {
+      edges {
+        node {
+          ... on User {
+            avatarUrl
+            id
+            login
+            url
+          }
+        }
+      }
+    }
+  }
+`;
 function App() {
-
+  const { loading: loadingUsers, data: userData } = useQuery(GET_USERS, {
+    variables: { userQuery: "gokhan" },
+  });
+  console.log(userData);
   return (
     <div className="App">
       <h1>GitHub Repositories</h1>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+  HttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+const httpLink = new HttpLink({
+  uri: "https://api.github.com/graphql",
+});
+const token = import.meta.env.VITE_GITHUB_TOKEN;
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${token}`,
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
-)
+  </ApolloProvider>
+);
